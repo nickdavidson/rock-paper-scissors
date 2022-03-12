aed3d1a3b3dddef97788821d35196bf792a0d281
Nick Davidson <nick.davidson@gmail.com>
2022-03-12 11:59:06 -0500

    Add playRound function to compare player and computer selections, returning a winner.

--- js/script.js
+++ js/script.js
@@ -13,9 +13,49 @@ function computerPlay() {
 }
 
 // Compare the two selections and determine the winner;
+function playRound(playerSelection, computerSelection){
+    let winner = "";
+    if(playerSelection === computerSelection){
+        winner = "draw";
+    }
+
+    else if (playerSelection === "rock"){
+        if (computerSelection === "paper"){
+            winner = "computer";
+        }
+        else if (computerSelection === "scissors"){
+            winner = "player";
+        }
+    }
+
+    else if (playerSelection === "paper"){
+        if (computerSelection === "scissors"){
+            winner = "computer";
+        }
+        else if (computerSelection === "rock"){
+            winner = "player";
+        }
+    }
+
+    else if (playerSelection === "scissors"){
+        if (computerSelection === "rock"){
+            winner = "computer";
+        }
+        else if (computerSelection === "paper"){
+            winner = "player";
+        }
+    }
+    else {
+        console.log("Invalid choice - please choose again.");
+    }
+
+    return winner;
+}
+
 // Do that 5 times, keeping score.
 
 const playerSelection = "";
 const computerSelection = "";
+
 
 // console.log(playerSelection());
