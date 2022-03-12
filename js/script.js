e889ff79034d7b1737abfc0878e5bdd3b31860f6
Nick Davidson <nick.davidson@gmail.com>
2022-03-12 11:04:01 -0500

    Add computerSelection function to generate CPU choice from array.

--- js/script.js
+++ js/script.js
@@ -3,12 +3,16 @@ console.log("Script is working");
 // User picks Rock, Paper, or Scissors (RPS);
 function playerSelection() {
     let playerSelection = prompt("Rock! Paper! Scissors!");
-    return playerSelection.toLowerCase();
+    return playerSelection.toLowerCase();   //return input normalized to lowercase
 }
 
 // Computer randomly generates RPS selection;
+function computerSelection() {
+    let cpuChoices = ["rock", "paper", "scisscors"];
+    return cpuChoices[Math.floor(Math.random()*cpuChoices.length)];     //randomly generate
+}
+
 // Compare the two selections and determine the winner;
 // Do that 5 times, keeping score.
 
-
-console.log(playerSelection());
+// console.log(playerSelection());
